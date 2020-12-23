354911e1ce76e0520dcec34c50d45466e50b57a7
Darshan Sheth <67975235+darshan-num@users.noreply.github.com>
2020-12-23 20:49:03 +0300

    Update add_msg_issue_prefix.py

--- add_msg_issue_prefix_hook/add_msg_issue_prefix.py
+++ add_msg_issue_prefix_hook/add_msg_issue_prefix.py
@@ -11,8 +11,36 @@ def main():
     try:
         branch = subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"], universal_newlines=True).strip()
         
-        if branch == "task_AA_attempt2":
-            raise SystemExit("Invalid branch name!")
+        branch_name = branch.split('_')
+        project_name = branch_name[1]
+        task_key = branch_name[2]
+
+        domain = "numerator"
+
+        issue_key = project_name + "-" + task_key
+        url = "https://" + domain + ".atlassian.net/rest/agile/1.0/issue/" + issue_key + ""
+
+        headers = {
+            "Accept": "application/json",
+            "Authorization": "Basic ZGFyc2hhbi5zaGV0aEBudW1lcmF0b3IuY29tOnFnS1ZmTFBaTVRiUnBZUU5DazFNOUVDNQ=="
+        }
+        connection = http.client.HTTPSConnection('www.atlassian.net')
+        connection.request("GET", url, headers=headers)
+        response = connection.getresponse().read()
+        reponse = (json.dumps(json.loads(response), sort_keys=True, indent=4, separators=(",", ": ")))
+        response = json.loads(response)
+
+        if response.get('errorMessages'):
+            raise SystemExit("Invalid branch name!" + "\nMessage:" + str(response.get('errorMessages')))
+
+        assignee = response.get("fields")["assignee"]["displayName"]
+        creator = response.get("fields")["creator"]["displayName"]
+        status = response.get("fields")["aggregateprogress"]["percent"]
+        print("==============")
+        print(
+            "Commiting to Jira task = " + issue_key + "\nAssigned to = " + assignee + "\nAssigned by = " + creator + "\nTask Status = " + str(
+                status) + "%")
+        print("==============")
     except Exception as e:
         print(e)
 if __name__ == "__main__":
