334d19358dba70019d455744265616141a6f47f6
Darshan Sheth <67975235+darshan-num@users.noreply.github.com>
2020-12-23 20:45:06 +0300

    Update add_msg_issue_prefix.py

--- add_msg_issue_prefix_hook/add_msg_issue_prefix.py
+++ add_msg_issue_prefix_hook/add_msg_issue_prefix.py
@@ -10,7 +10,9 @@ def main():
     branch = ""
     try:
         branch = subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"], universal_newlines=True).strip()
-        print(commit_msg_filepath, branch)
+        
+        if branch == "task_AA_attempt2":
+            raise SystemExit("Invalid branch name!")
     except Exception as e:
         print(e)
 if __name__ == "__main__":
