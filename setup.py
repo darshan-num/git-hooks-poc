82e16df721f85d214f1a3811d8c6d58c9707712f
Darshan Sheth <67975235+darshan-num@users.noreply.github.com>
2020-12-23 13:44:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
-setup()
+setup(name='hooks',
+      version='1.0',
+      description='Python hook',
+      url='https://github.com/darshan-num/git-hooks-poc/tree/githook',
+      packages=['hook'],)
  
