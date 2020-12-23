e5ba301cc5f2d7826cc427cdbd7a78e30c950ff3
Darshan Sheth <67975235+darshan-num@users.noreply.github.com>
2020-12-23 20:33:43 +0300

    Update add_msg_issue_prefix.py

--- add_msg_issue_prefix_hook/add_msg_issue_prefix.py
+++ add_msg_issue_prefix_hook/add_msg_issue_prefix.py
@@ -4,36 +4,7 @@ import sys
 import re
 import subprocess
 
-
-def get_ticket_id_from_branch_name(branch):
-    matches = re.findall('[a-zA-Z]{1,10}-[0-9]{1,5}', branch)
-    if len(matches) > 0:
-        return matches[0]
-
-
 def main():
-    commit_msg_filepath = sys.argv[1]
-
-    branch = ""
-    try:
-        branch = subprocess.check_output(["git","symbolic-ref", "--short", "HEAD"], universal_newlines=True).strip()
-    except Exception as e:
-        print(e)
-
-    result = get_ticket_id_from_branch_name(branch)
-    issue_number = ""
-
-    if result:
-        issue_number = result.upper()
-
-    with open(commit_msg_filepath, "r+") as f:
-        content = f.read()
-        f.seek(0, 0)
-        if issue_number:
-            f.write("[{}] {}".format(issue_number, content))
-        else:
-            f.write(content)
-
-
+    print("hello")
 if __name__ == "__main__":
     exit(main())
